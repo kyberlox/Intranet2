188e8247a6600312fd277af926b49b1f9a0e45d2
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-06-09 12:56:48 +0400

    обходим CORS

--- code/src/services/Auth.py
+++ code/src/services/Auth.py
@@ -230,7 +230,7 @@ class AuthService:
 
 
 @auth_router.post("/auth")
-async def authentication(data = Body(), response : Response): #login : str, password : str,
+async def authentication(data : Body, response : Response): #login : str, password : str,
     login = data["login"]
     password = data["password"]
     session = await AuthService().authenticate(login, password)

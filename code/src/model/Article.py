33a95c2e5dbd25a7473856fdd600ea5718d861c2
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-06-19 13:14:28 +0400

    отладка файлов

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -288,6 +288,7 @@ class Article:
                         #sprint(f'{f_id} файл добавлен в монго', art_id, inf_id)
                         files_data.append(file_data)
                     except:
+                        LogsMaker().warning_message(f"Не получилось по хорошему скачать файл {f_id} статьи {art_id} инфоблока {inf_id}, метода Матренина по умолчанию - {need_all_method}")
                         is_preview = f_id in preview_images
                         file_data = File(b24_id=f_id).upload_inf_art(art_id, is_preview, True, inf_id)
                         # sprint(f'{f_id} файл добавлен в монго', art_id, inf_id)

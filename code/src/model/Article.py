18b24f3934ba9b36b56a03ba15b4986e4ea0aca0
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-06-23 10:21:12 +0400

    компрессия файлов

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -693,9 +693,9 @@ class Article:
             if file["is_preview"]:
                 url = file["file_url"]
                 #внедряю компрессию
-                preview_link = url.split("/")
-                preview_link[-2] = "compress_image"
-                url = '/'.join(preview_link)
+                # preview_link = url.split("/")
+                # preview_link[-2] = "compress_image"
+                # url = '/'.join(preview_link)
                 #!!!!!!!!!!!!!!!!!!временно исправим ссылку!!!!!!!!!!!!!!!!!
                 return f"http://intranet.emk.org.ru{url}"
                 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -705,9 +705,9 @@ class Article:
             if "image" in file["content_type"] or "jpg" in file["original_name"] or "jpeg" in file["original_name"] or "png" in file["original_name"]:
                 url = file["file_url"]
                 #внедряю компрессию
-                preview_link = url.split("/")
-                preview_link[-2] = "compress_image"
-                url = '/'.join(preview_link)
+                # preview_link = url.split("/")
+                # preview_link[-2] = "compress_image"
+                # url = '/'.join(preview_link)
                 #!!!!!!!!!!!!!!!!!!временно исправим ссылку!!!!!!!!!!!!!!!!!
                 return f"http://intranet.emk.org.ru{url}"
                 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

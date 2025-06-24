87b2bbad106efa7d299ee0a8bd8538d9110b9427
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-06-24 09:54:00 +0400

    Доска почета

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -152,7 +152,7 @@ class Article:
             
             print(uuid)
 
-            user = User(id=uuid).search_by_id()
+            user = User(id=uuid).search_by_id().__dict__
             print(user)
             photo = user["photo_file_url"]
             indirect_data = json.dumps({

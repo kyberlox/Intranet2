f4232f62ab23884253464d616d199ccafe2bdb25
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-06-26 12:08:32 +0400

    Блоги

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -219,7 +219,15 @@ class Article:
 
             #отдельно обрабатываем файлы
             if "PROPERTY_457" in data:
-                content = take_value(data["PROPERTY_457"])['TEXT']
+                if "TEXT" in data[key]:
+                        content = list(data[key]["TEXT"].values())[0]
+                        if "TYPE" in data[key]:
+                            content_type = list(data[key]["TYPE"].values())[0]
+
+                    elif "TEXT" in list(data[key].values())[0]:
+                        content = list(data[key].values())[0]["TEXT"]
+                        if "TYPE" in list(data[key].values())[0]:
+                            content_type = list(data[key].values())[0]["TYPE"]
             elif "PROPERTY_1239" in data:
                 content = take_value(data["PROPERTY_1239"])
             #хватаю url

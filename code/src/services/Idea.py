37d7a7cca900f66a1aa44e7993004f57010deb54
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-07-08 14:44:53 +0300

    Есть Идея

--- code/src/services/Idea.py
+++ code/src/services/Idea.py
@@ -11,12 +11,13 @@ from src.service.Auth import AuthService
 class Idea:
     def __init__(self, user_id=None, user_uuid=None):
         b24_ideas = Article(section_id=121).get_inf()
-
-        self.auth = AuthService()
+        
+        self.user_uuid = None
+        self.username = None
 
     def get_user(self, session_id):
-        self.user = self.auth.get_user_by_seesion_id(session_id)
+        self.user = AuthService().get_user_by_seesion_id(session_id)
 
         if self.user is not None:
-            user_uuid = self.user["user_uuid"]
-            user_uuid = self.user["user_uuid"]
+            self.user_uuid = self.user["user_uuid"]
+            self.username = self.user["username"]

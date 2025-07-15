4d18bc637e83c515b814f4350322bc15731de025
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-07-15 09:16:27 +0300

    Редакторка

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -56,7 +56,7 @@ class Editor:
                         "name" : self.fields[k],
                         "value" : art[k],
                         "field" : k,
-                        "data_type" : type(art[k])
+                        #"data_type" : type(art[k])
                         })
                 elif k in art["indirect_data"]:
                     field.append({

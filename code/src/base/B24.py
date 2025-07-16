72be6ee5c6f4e206267098874455136920ddab33
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-07-16 14:44:14 +0300

    Проксирую запросы в битру

--- code/src/base/B24.py
+++ code/src/base/B24.py
@@ -77,15 +77,18 @@ class B24:
 
         #url = "https://portal.emk.ru/rest/1/aj7d42rcogl2f51b/lists.element.add?IBLOCK_TYPE_ID=lists&IBLOCK_ID=121&ELEMENT_CODE=test3&FIELDS%5BPROPERTY_1049%5D=test_number&FIELDS%5BNAME%5D=test_name&FIELDS%5BDETAIL_TEXT%5D=test_text&FIELDS%5BCREATED_BY%5D=user_id&FIELDS%5BPROPERTY_1027%5D=test_file"
         self.bx24 = Bitrix24("https://portal.emk.ru/rest/1/aj7d42rcogl2f51b/")
-        payload = {
+        params = {
             "IBLOCK_TYPE_ID" : "list",
             "IBLOCK_ID" : 121,
             "ELEMENT_CODE" : el_code,
             "FIELDS" : fields
         }
-        result = self.bx24.callMethod('lists.element.add', payload)
-        
-        return result
+        try:
+            result = self.bx24.callMethod('lists.element.add', params)
+            print(f"Элемент успешно добавлен с ID: {result['ELEMENT_ID']}")
+            return result
+        except Exception as e:
+            print(f"Не удалось добавить элемент: {e}")
         #https://portal.emk.ru/rest/1/p6653nbau95j5a0h/bizproc.workflow.start?TEMPLATE_ID=2216&DOCUMENT_ID[]=lists&DOCUMENT_ID[]=Bitrix\Lists\BizprocDocumentLists&DOCUMENT_ID[]=$ID
 
 @b24_router.get("/calendar/{date_from}/{date_to}")

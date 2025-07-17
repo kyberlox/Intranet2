7b25da00e77a2ff4968ea02dbe8c8899538a719f
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-07-17 12:03:13 +0300

    Проксирую запросы в битру

--- code/src/base/B24.py
+++ code/src/base/B24.py
@@ -98,6 +98,7 @@ class B24:
         uid = fields["CREATED_BY"]
         if "base" in fields:
             base = fields["base"]
+            base_name = fields["base_name"]
             url = f"https://portal.emk.ru/rest/1/aj7d42rcogl2f51b/lists.element.add?IBLOCK_TYPE_ID=lists&IBLOCK_ID=121&ELEMENT_CODE={incr}&FIELDS[PROPERTY_1049]={incr}&FIELDS[NAME]={name}&FIELDS[DETAIL_TEXT]={cont_text}&FIELDS[CREATED_BY]={uid}&FIELDS[PROPERTY_1027][fileName]={base_name}&FIELDS[PROPERTY_1027][fileData]={base}"
         else:
             url = f"https://portal.emk.ru/rest/1/aj7d42rcogl2f51b/lists.element.add?IBLOCK_TYPE_ID=lists&IBLOCK_ID=121&ELEMENT_CODE={incr}&FIELDS[PROPERTY_1049]={incr}&FIELDS[NAME]={name}&FIELDS[DETAIL_TEXT]={cont_text}&FIELDS[CREATED_BY]={uid}"

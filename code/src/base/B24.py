8e4c9da1b0077a9f34001cad21ac5593f592f8e3
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-07-18 11:32:31 +0300

    Проксирую запросы в битру

--- code/src/base/B24.py
+++ code/src/base/B24.py
@@ -133,6 +133,7 @@ class B24:
             response  = requests.post(api_url, data=data, headers=headers)
         
             return response.json()
+            
         else:
             url = f"https://portal.emk.ru/rest/1/aj7d42rcogl2f51b/lists.element.add?IBLOCK_TYPE_ID=lists&IBLOCK_ID=121&ELEMENT_CODE={incr}&FIELDS[PROPERTY_1117]=909&FIELDS[PROPERTY_1049]={incr}&FIELDS[NAME]={name}&FIELDS[DETAIL_TEXT]={cont_text}&FIELDS[CREATED_BY]={uid}"
             print(url)

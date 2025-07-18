777b382c262a19e89bca75e6ecc8096f631aac6a
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-07-18 13:16:59 +0300

    Проксирую запросы в битру

--- code/src/base/B24.py
+++ code/src/base/B24.py
@@ -138,11 +138,10 @@ class B24:
             print(url)
             response = requests.get(url)
         
-        print(response)
         ID = response.json()['result']
             
         bis_url = f"https://portal.emk.ru/rest/1/p6653nbau95j5a0h/bizproc.workflow.start?TEMPLATE_ID=2216&DOCUMENT_ID[]=lists&DOCUMENT_ID[]=Bitrix\Lists\BizprocDocumentLists&DOCUMENT_ID[]={ID}"
-        return bis_url
+        return ID
 
 
 @b24_router.get("/calendar/{date_from}/{date_to}")

9200d32dadc0d87fa90c62568b57aa1624be9b4d
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-07-21 16:06:01 +0300

    Актуальные новости и Корпоративные события

--- clear.sh
+++ clear.sh
@@ -5,7 +5,8 @@ docker-compose down
 docker system prune -a
 
 #чистим файлы и базы данных
-rm -r ./code/files_db/user_photo
+rm -r ./code/files_db/
+mkdir ./code/files_db
 mkdir ./code/files_db/user_photo
 
 rm -r ./mongodb/data/*

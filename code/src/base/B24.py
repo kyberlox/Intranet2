2fb81b5a29e0c81af50aa639b7aefdc03d81c9c0
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-07-29 14:44:30 +0300

    ускоряю колендарь

--- code/src/base/B24.py
+++ code/src/base/B24.py
@@ -154,11 +154,11 @@ class B24:
 
 @b24_router.get("/calendar/{date_from}/{date_to}")
 def calendar_event(date_from, date_to):
-    '''
+    
     url = f"https://portal.emk.ru/rest/1/f5ij1aoyuw5f39nb/calendar.event.get.json?type=company_calendar&ownerId=0&from={date_from}&to={date_to}"
     response = requests.get(url)
     result = response.json()
     return result
-    '''
-    return B24().get_calendar(date_from, date_to)
+    
+    #return B24().get_calendar(date_from, date_to)
     

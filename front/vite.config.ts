d7d635a2248c4c395cb183af02347bfc7fd15c38
Fullofb3ans <igorgazinskii6@gmail.com>
2025-08-05 14:01:49 +0400

    bugявно указал сервер

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -38,9 +38,11 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: ['http://intranet1-dmz.imp.int/', 'http://testrcube1-a66.imp.int/']
   },
   preview: {
     host: "0.0.0.0",
     port: 5173,
-  }
+  },
+
 })

1dbab0589564d05b98dafa1982794cc2da70654f
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-08-05 17:06:45 +0400

    деплой демки на сервер

--- code/src/services/Auth.py
+++ code/src/services/Auth.py
@@ -79,9 +79,9 @@ class AuthService:
             #ID=user_data.get("ID", ""),
             ID="2375",
             #email=user_data.get("email", ""),
-            email=""
+            email="",
             #full_name=user_data.get("full_name", ""),
-            full_name="Инкогнито Челик"
+            full_name="Инкогнито Челик",
             expires_at=dt.strftime('%Y-%m-%d %H:%M:%S')
         ).dict()
 

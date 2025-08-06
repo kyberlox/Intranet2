d8cbfe3f1ac4fd71c3e25aae5b46ae13a76eef66
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-08-06 15:01:19 +0400

    деплой демки на сервер

--- code/src/base/SearchModel.py
+++ code/src/base/SearchModel.py
@@ -28,8 +28,8 @@ search_router = APIRouter(prefix="/elastic", tags=["Поиск по тексту
 
 elastic_client = Elasticsearch(hosts=[f'{DOMAIN[:-5]}:9200'], basic_auth=('elastic', pswd), verify_certs=False)
 elastic_client1 = Elasticsearch(hosts=[f'{DOMAIN[:-5]}:9200'], http_auth=('elastic', pswd), verify_certs=False)
-elastic_client = Elasticsearch(hosts=['http://elasticsearch:9200'], basic_auth=('elastic', pswd), verify_certs=False)
-elastic_client1 = Elasticsearch(hosts=['http://elasticsearch:9200'], http_auth=('elastic', pswd), verify_certs=False)
+elastic_client2 = Elasticsearch(hosts=['http://elasticsearch:9200'], basic_auth=('elastic', pswd), verify_certs=False)
+elastic_client3 = Elasticsearch(hosts=['http://elasticsearch:9200'], http_auth=('elastic', pswd), verify_certs=False)
 
 if elastic_client.ping():
     print("✅ Успешное подключение!")
@@ -37,6 +37,15 @@ else:
     print("❌ Ошибка аутентификации!")
 
 if elastic_client1.ping():
+    print("✅ Успешное подключение!")
+else:
+    print("❌ Ошибка аутентификации!")
+if elastic_client2.ping():
+    print("✅ Успешное подключение!")
+else:
+    print("❌ Ошибка аутентификации!")
+
+if elastic_client3.ping():
     print("✅ Успешное подключение!")
 else:
     print("❌ Ошибка аутентификации!")

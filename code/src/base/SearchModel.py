183a5b6643039c86092990ae31d8f73e053c353b
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-08-06 13:59:17 +0400

    деплой демки на сервер

--- code/src/base/SearchModel.py
+++ code/src/base/SearchModel.py
@@ -24,7 +24,7 @@ DOMAIN = os.getenv('DOMAIN')
 
 search_router = APIRouter(prefix="/elastic", tags=["Поиск по тексту"])
 
-elastic_client = Elasticsearch(hosts=['http://elastic:9200'], http_auth=('elastic', pswd), verify_certs=False)
+elastic_client = Elasticsearch(hosts=[f'{DOMAIN[:-5]}:9200'], http_auth=('elastic', pswd), verify_certs=False)
 
 
 

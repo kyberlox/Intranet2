07554afaee27b9ff1bdc962557ac5ae0bd67e0fc
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-08-07 12:01:05 +0400

    деплой демки на сервер

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -114,10 +114,9 @@ class Article:
             content = list(data['PROPERTY_365'].values())[0]
             # data.pop('PROPERTY_365')
         elif "PROPERTY_374" in data:
-            print(data["PROPERTY_374"])
             content = list(data['PROPERTY_374'].values())[0]["TEXT"]
             content_type = list(data['PROPERTY_374'].values())[0]["TYPE"]
-            print(content)
+
         else:
             keys = ["PROPERTY_1239", "PROPERTY_457", "PROPERTY_477", "PROPERTY_340", "PROPERTY_291", "PROPERTY_358", "PROPERTY_1034", "PROPERTY_348"]
             content = None

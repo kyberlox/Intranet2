ec540018ba5cf7cdc2cf59344e539d709c4ee013
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-08-08 11:31:13 +0400

    деплой демки на сервер

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -1839,7 +1839,7 @@ class Article:
             return idea_block
         
         #конкурсы
-        elif section_id == 7:\
+        elif section_id == 7:
             print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             articles_in_section = ArticleModel(section_id=section_id).find_by_section_id()
             images = []

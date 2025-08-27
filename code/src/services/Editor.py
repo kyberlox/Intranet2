f281caa9c4b78eab9d820ba00fdd398148b50e84
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-08-27 16:45:10 +0400

    почти готовая демка

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -305,6 +305,7 @@ class Editor:
 
                 #если это часть indirect_data
                 else:
+                    if "indirect_data" in art and art["indirect_data"] is not None: 
                     art["indirect_data"][key] = data[key]
         
         print(art)

e690bff4cb2e0aba839283720f87e1057e6e0443
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-08-28 12:14:56 +0400

    почти готовая демка

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -424,7 +424,7 @@ async def render(art_id : int):
 
 ### тестирую работу с файлами
 @editor_router.post("/upload_file/{art_id}")
-def create_file(file: UploadFile, art_id : int):
+def create_file(file: UploadFile, art_id : int): #нельзя асинхронить
     # Здесь нужно сохранить файл или обработать его содержимое
     f_inf = storeFile(art_id = int(art_id)).editor_add_file(file=file)
     return f_inf

faa205093c90a6912ae48e613fd427f59eb731e7
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-08-28 16:03:59 +0400

    почти готовая демка

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -49,7 +49,7 @@ class Editor:
         self.fundamental = ["id, section_id", "name", "content_text", "content_type", "active", "date_publiction", "date_creation", "preview_text"]
 
         self.notEditble = ["id", "section_id", "date_creation", "content_type"]
-        if self.section_id in [18, 52, 54, 172] :
+        if self.section_id in ["18", 52, 54, 172] :
                 self.notEditble.append("preview_text")
 
         self.variable = {

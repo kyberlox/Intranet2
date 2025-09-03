79b58c37a2127d9847acb516548de29f5193ff83
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-09-03 12:45:58 +0400

    почти готовая демка

--- code/src/model/File.py
+++ code/src/model/File.py
@@ -448,7 +448,7 @@ class File:
 
 
 
-    async def editor_add_file(self, file : webFile):
+    def editor_add_file(self, file : webFile):
         #!!!!!!!внедрить проверки
         
         # Генерируем уникальное имя файла
@@ -461,15 +461,15 @@ class File:
         
 
         # Если нужно сохранить файл на диск
-        # with file.file:
-        #     contents = file.file.read()
-        #     with open(file_path, "wb") as f:
-        #         f.write(contents)
-        
         with file.file:
             contents = file.file.read()
-            async with aiofiles.open(file_path, "wb") as f:
-                await f.write(contents)
+            with open(file_path, "wb") as f:
+                f.write(contents)
+        
+        # with file.file:
+        #     contents = file.file.read()
+        #     async with aiofiles.open(file_path, "wb") as f:
+        #         await f.write(contents)
 
         file_info = {
             "original_name": filename,

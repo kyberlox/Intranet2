c423ab0b15e43e65590cba3891b7a36492d3747d
maxkucher <maxkucher@WIN-9JUMEIJVHNL.localdomain>
2025-09-10 13:51:40 +0400

    отладка

--- code/src/base/SearchModel.py
+++ code/src/base/SearchModel.py
@@ -1406,7 +1406,7 @@ class ArticleSearchModel:
             section_href = next((s.get('sectionHref') for s in sections if s['id'] == res_info["_source"]["section_id"]), res_info["_source"]["section_id"])
             art_info['sectionHref'] = section_href
             art_info['id'] = int(res_info["_id"])
-            if "authorId" in res_info["_source"].keys():
+            if "authorId" in res_info["_source"].keys() and res_info["_source"]["authorId"] is not None:
                 art_info['authorId'] = res_info["_source"]["authorId"]
             art_info['image'] = res_info["_source"]["preview_photo"]
             art_info['coincident'] = res_info['highlight']

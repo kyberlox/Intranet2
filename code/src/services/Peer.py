4134e0cc4d13b68ce36aa5ad7bbed4d87336dbe2
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2025-09-25 16:52:27 +0400

    отладка MAXa

--- code/src/services/Peer.py
+++ code/src/services/Peer.py
@@ -27,8 +27,10 @@ class Peer:
         from ..base.pSQL.objects.PeerUserModel import PeerUserModel
         self.PeerUserModel = PeerUserModel()
 
-        self.Roots = self.RootsModel(user_uuid=self.user_uuid).get_token_by_uuid()
-        self.roots = self.RootsModel(user_uuid=self.user_uuid).token_processing_for_vision(self.Roots)
+
+        self.RootsModel.user_uuid = self.user_uuid
+        self.Roots = self.RootsModel.get_token_by_uuid()
+        self.roots = self.RootsModel.token_processing_for_vision(self.Roots)
     
     """Ручки которые доступны любому пользователю"""
     def sum(self): 

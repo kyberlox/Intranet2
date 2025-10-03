a47b6568b582f8656e172201020287e87baac48a
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-10-03 18:12:23 +0400

    отлаживаю конфиг запуска сервера

--- reset.sh
+++ reset.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git pull origin main
-docker-compose down fastapi
-docker-compose up -d fastapi
-#docker-compose restart fastapi
+#docker-compose down fastapi
+#docker-compose up -d fastapi
+docker-compose restart fastapi
 docker-compose logs -f fastapi

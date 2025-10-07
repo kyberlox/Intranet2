08ca2d940323f9c89167e622c79a891abbde03e4
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-10-07 12:00:44 +0300

    Админка с доступом к портам

--- code/src/services/AdminPanel.py
+++ code/src/services/AdminPanel.py
@@ -1,12 +1,31 @@
 from src.services.LogsMaker import LogsMaker
 from src.services.Auth import LogsMaker
 
-def check_and_add():
-    #беру логин и пароль
-    #проверяю валидность
+def check_and_add(username, password, ip_adress):
+    #проверяю валидность на логин и пароль
+    Auth().check_admin_credentials(username, password)
 
-    #получаю ip 
+    #проверяю ip адрес
+    need_update_wirewall = add_ip(ip_adress)
 
-    #проверяю 
+    #перезапускаю правила
+    if need_update_wirewall:
 
-def add_ip
+
+def add_ip(ip_adress):
+    #собираю с список ip адреса
+    with open("../../../firewall/admin_ip.txt") as adm_ip_file:
+        adm_ip = adm_ip_file.readlines()
+    
+    if ip_adress in adm_ip:
+        return False
+    else:
+        #записываю в конец
+        with open('file.txt', 'a') as adm_ip_file:  
+            adm_ip_file.write(ip_adress + '\n')  
+        return True
+
+add_ip("217.65.222.242")
+
+add_ip("1.1.1.1")
+

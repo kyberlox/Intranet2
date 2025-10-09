454b3dd4f48ed47ee3c9cb700623cf5e07fd338c
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2025-10-09 15:07:21 +0400

    отладка MAXa

--- code/src/services/SendMail.py
+++ code/src/services/SendMail.py
@@ -88,8 +88,8 @@ class SendEmail:
             """
             
             msg.attach(MIMEText(content, "html"))
-            # file_id = self.file_url.split('.') /intranet/Intranet2/code/files_db
-            # image = file_id[0]
+            file_id = self.file_url.split('/') #/intranet/Intranet2/code/files_db
+            self.file_url = file_id[-1]
             file_path = os.path.join(STORAGE_PATH, self.file_url)
             with open(file_path, "rb") as img_file: 
                 logo = MIMEImage(img_file.read())

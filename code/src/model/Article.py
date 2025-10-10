eea3fe3ce97d26c2af4ebfa383f4d40d51239fa5
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-10-10 15:30:48 +0300

    орк развитие

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -1139,7 +1139,7 @@ class Article:
         ! Сопоставить section_id из Интранета и IBLOCK_ID из B24
         '''
 
-        #self.upload_uniquely()
+        self.upload_uniquely()
         self.upload_with_parameter()
         # self.upload_many_to_many()
         # self.upload_services()

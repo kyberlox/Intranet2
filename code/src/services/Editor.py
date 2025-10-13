c537875b166281ed6cef74496b663b08b8ec5102
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-10-13 13:12:42 +0300

    Редактор Блогов

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -462,7 +462,7 @@ class Editor:
         art = Article(id = self.art_id).find_by_id()
 
         if self.section_id is None:
-            if section_id in art:
+            if "section_id" in art:
                 self.section_id = art["section_id"]
             else:
                 return LogsMaker.warning_message("Неверный id статьи")

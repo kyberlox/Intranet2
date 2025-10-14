d38a4b29c9addbad2c26e217ea61d66ce6db21be
Fullofb3ans <igorgazinskii6@gmail.com>
2025-10-14 09:36:05 +0400

    bug/tourBugfix

--- front/src/views/gallery/factoryGuid/FactoryTour.vue
+++ front/src/views/gallery/factoryGuid/FactoryTour.vue
@@ -1,21 +1,26 @@
 <template>
-<div v-if="tourFrame" class="pano__wrapper mt20">
-         <iframe :src="tourFrame" width="100%" height="100%" frameborder="0"></iframe>
+<div v-if="tourFrame"
+     class="pano__wrapper mt20">
+    <iframe :src="tourFrame"
+            width="100%"
+            height="100%"
+            frameborder="0"></iframe>
 </div>
-<div v-else>
-    <Loader/>
+<div v-else
+     class="contest__page__loader__wrapper">
+    <Loader class="contest__page__loader" />
 </div>
 </template>
 
 <script lang="ts">
-import { loadScript} from "vue-plugin-load-script";
+import { loadScript } from "vue-plugin-load-script";
 import { ref, computed, watch } from 'vue';
 import { defineComponent } from 'vue';
 import { useFactoryGuidDataStore } from "@/stores/factoryGuid";
 import Loader from "@/components/layout/Loader.vue";
 
 export default defineComponent({
-    components:{
+    components: {
         Loader
     },
     props: {
@@ -27,18 +32,18 @@ export default defineComponent({
         }
     },
     setup(props) {
-         loadScript('/src/utils/tour.js')
+        // loadScript('/src/utils/tour.js')
         const factoryGuid = useFactoryGuidDataStore();
         const currentTour = computed(() => factoryGuid.getFactoryTour(Number(props.factoryId), String(props.id)))
         const tourFrame = ref();
 
-        watch((currentTour), ()=>{
-            if(currentTour.value){
-         tourFrame.value = `https://intranet.emk.ru/api/files/tours/${currentTour.value?.["3D_files_path"]}/index.html`;
+        watch((currentTour), () => {
+            if (currentTour.value) {
+                tourFrame.value = `https://intranet.emk.ru/api/files/tours/${currentTour.value?.["3D_files_path"]}/index.html`;
             }
-        }, {immediate: true, deep: true})
+        }, { immediate: true, deep: true })
 
-        return{
+        return {
             tourFrame
         }
     }

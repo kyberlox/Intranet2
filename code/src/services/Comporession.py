fd4700a1cbb492726ce1d1d6568654eea2273448
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2025-10-17 15:09:30 +0400

    отладка MAXa

--- code/src/services/Comporession.py
+++ code/src/services/Comporession.py
@@ -41,7 +41,7 @@ def resize_image_quality(input_path: str) -> BytesIO:
         )
         
         # Легкое повышение резкости (опционально)
-        img = img.filter(ImageFilter.SHARPEN)
+        # img = img.filter(ImageFilter.SHARPEN)
         
         # Сохранение с высоким качеством
         output_buffer = BytesIO()

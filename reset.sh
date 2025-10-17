c321510384e2afd883d2154236bd1c94f248c75d
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-10-17 10:33:34 +0300

    конфиги для dev

--- reset.sh
+++ reset.sh
@@ -4,10 +4,10 @@ git pull origin dev
 #docker-compose down fastapi
 #docker-compose up -d fastapi
 
-#docker-compose restart fastapi
+docker-compose restart fastapi
 
-docker-compose stop fastapi
-docker-compose rm fastapi
-docker-compose up -d fastapi
+#docker-compose stop fastapi
+#docker-compose rm fastapi
+#docker-compose up -d fastapi
 
 docker-compose logs -f fastapi

f76c9a1e89fde1c1827540b75533c244440014ea
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-10-21 23:48:45 +0300

    новое файловое хранилище

--- code/src/model/File.py
+++ code/src/model/File.py
@@ -493,6 +493,7 @@ class File:
             ) #вернет False, если пытаться скачать актуальную фотку ещё раз
             if w_photo is not False:
                 #скачать файл
+                print(b24_url, w_photo['name'])
                 result = self.dowload_user_photo(url=b24_url, name=w_photo['name'])
             print(w_photo)
             return w_photo

731ea25ab040d6a7af7c3fb6266e8ba7e69545ad
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-11-13 13:30:17 +0300

    Проверяю дамп

--- code/src/model/User.py
+++ code/src/model/User.py
@@ -42,9 +42,6 @@ class User:
 
         #отправить записи
         for usr_data in logg.progress(data, "Обработка информации о пользователях "):
-
-            if int(usr_data["ID"]) <= 100:
-
                 await self.UserModel.upsert_user(user_data=usr_data, session=session)
                 await session.commit()
             
@@ -95,8 +92,6 @@ class User:
         logg = LogsMaker()
         for usr_data in logg.progress(data, "Загрузка фотографий пользователей "):
             #найдем фото пользователя, если у пользователя есть аватарка
-
-            if "ID" in usr_data and int(usr_data["ID"]) <= 100:
 
                 #if usr_data['ID'] in cool_users:
                 uuid = usr_data['ID']

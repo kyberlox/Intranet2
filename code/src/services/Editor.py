2b16050b0f154cb686d46c4d0b17e527d9670700
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-11-17 10:13:04 +0300

    асинхронный редактор

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -77,7 +77,7 @@ class Editor:
     async def validate(self):
         if self.art_id is not None and section_id is None:
             
-            art = await ArticleModel(id = self.art_id).find_by_id(session=self.session))
+            art = await ArticleModel(id = self.art_id).find_by_id(session=self.session)
             # art = asyncio.run(ArticleModel(id = self.art_id).find_by_id(session=self.session))
             
             if "section_id" in art:

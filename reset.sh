90c17f630de38c966e2190b8644ab6d2ed48a51d
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-03 16:39:19 +0300

    Кастомизация /docs

--- reset.sh
+++ reset.sh
@@ -9,5 +9,6 @@ git pull origin main
 docker-compose stop fastapi
 docker-compose rm fastapi
 docker-compose up -d fastapi
+#docker-compose up -d --build fastapi #если требуется установка библиотек
 
 docker-compose logs -f fastapi

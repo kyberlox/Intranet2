c6b170a897960d315e64e5ba11e7c2aa2b197d2b
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-04 09:45:53 +0300

    Кастомизация /docs

--- code/src/base/B24.py
+++ code/src/base/B24.py
@@ -184,14 +184,192 @@ description="""
     - Возвращает события в формате JSON
     - Поддерживает фильтрацию по датам
     
-    ### Параметры:
+    ---
+
+    ### Входные параметры:
     - **date_from**: Начальная дата периода (формат: ГГГГ-ММ-ДД)
     - **date_to**: Конечная дата периода (формат: ГГГГ-ММ-ДД)
+
+    ### Выходные данные:
+    - "ID": string,
+    - "PARENT_ID": string,
+    - "ACTIVE": string,
+    - "DELETED": string,
+    - "CAL_TYPE": string,
+    - "OWNER_ID": string,
+    - "NAME": string,
+    - "DATE_FROM": string,
+    - "DATE_TO": string,
+    - "EVENT_TYPE": null,
+    - "CREATED_BY": string,
+    - "DATE_CREATE": string,
+    - "TIMESTAMP_X": string,
+    - "DESCRIPTION": string,
+    - "PRIVATE_EVENT": string,
+    - "ACCESSIBILITY": string,
+    - "IMPORTANCE": string,
+    - "IS_MEETING": boolean,
+    - "MEETING_STATUS": string,
+    - "MEETING_HOST": string,
+    - "MEETING": {
+            "NOTIFY": boolean,
+            "MEETING_CREATOR": number,
+            "REINVITE": boolean,
+            "ALLOW_INVITE": boolean,
+            "HIDE_GUESTS": boolean,
+            "HOST_NAME": string,
+        },
+    - "LOCATION": string,
+    - "COLOR": string,
+    - "TEXT_COLOR": string,
+    - "DATE_FROM_FORMATTED": string,
+    - "DATE_TO_FORMATTED": string,
+    - "ATTENDEE_LIST": {
+            "id": number,
+            "entryId": string,
+            "status": string
+        }[],
+    - "attendeesEntityList":
+        {
+            "entityId": string,
+            "id": number
+        }[],
+    - "~DATE_FROM": string,
+    - "~DATE_TO": string,
     
+    ---
+
     ### Примеры использования:
+
+    #### Запрос:
     ```
-    GET /calendar/2024-01-01/2024-01-31
+    GET /calendar/31.10.2025/31.10.2025
+    ```
+
+    #### Вывод
+    ```
+    {
+    "result": [
+    {
+        "ID": "193693",
+        "PARENT_ID": "193693",
+        "ACTIVE": "Y",
+        "DELETED": "N",
+        "CAL_TYPE": "company_calendar",
+        "OWNER_ID": "0",
+        "NAME": "VR-игра",
+        "DATE_FROM": "31.10.2025 18:00:00",
+        "DATE_TO": "31.10.2025 21:00:00",
+        "ORIGINAL_DATE_FROM": null,
+        "TZ_FROM": "Europe/Saratov",
+        "TZ_TO": "Europe/Saratov",
+        "TZ_OFFSET_FROM": "14400",
+        "TZ_OFFSET_TO": "14400",
+        "DATE_FROM_TS_UTC": "1761904800",
+        "DATE_TO_TS_UTC": "1761915600",
+        "DT_SKIP_TIME": "N",
+        "DT_LENGTH": 10800,
+        "EVENT_TYPE": null,
+        "CREATED_BY": "2857",
+        "DATE_CREATE": "17.10.2025 10:34:30",
+        "TIMESTAMP_X": "17.10.2025 10:34:30",
+        "DESCRIPTION": "Это предварительное оповещение о предстоящем мероприятии, даты и время могут быть скорректированы.\r\nБолее подробная информация по всем условиям участия и ЗАПИСЬ на мероприятие будут анонсированы в рассылках и афише.\r\nПо всем вопросам просьба обращаться в Отдел внутрикорпоративных коммуникаций [URL=mailto:ovk@emk.ru]ovk@emk.ru[/URL]",
+        "DT_FROM": null,
+        "DT_TO": null,
+        "PRIVATE_EVENT": "",
+        "ACCESSIBILITY": "busy",
+        "IMPORTANCE": "normal",
+        "IS_MEETING": true,
+        "MEETING_STATUS": "H",
+        "MEETING_HOST": "2857",
+        "MEETING": {
+            "NOTIFY": false,
+            "MEETING_CREATOR": 2857,
+            "REINVITE": false,
+            "ALLOW_INVITE": false,
+            "HIDE_GUESTS": true,
+            "HOST_NAME": "Анжелика Рожкова",
+            "LANGUAGE_ID": null,
+            "MAIL_FROM": false,
+            "CHAT_ID": 0
+        },
+        "LOCATION": "Deep VR",
+        "REMIND": [
+            {
+                "type": "daybefore",
+                "before": 0,
+                "time": 540
+            }
+        ],
+        "COLOR": "",
+        "TEXT_COLOR": "",
+        "RRULE": "",
+        "EXDATE": "",
+        "DAV_XML_ID": "20251031T140000Z-a568e215749928d1bf5c065b81d48e50@portal.emk.ru",
+        "G_EVENT_ID": "",
+        "DAV_EXCH_LABEL": "",
+        "CAL_DAV_LABEL": "",
+        "VERSION": "1",
+        "ATTENDEES_CODES": [
+            "U2857",
+            "UA"
+        ],
+        "RECURRENCE_ID": null,
+        "RELATIONS": "",
+        "SECTION_ID": "1949",
+        "SYNC_STATUS": null,
+        "UF_CRM_CAL_EVENT": false,
+        "UF_WEBDAV_CAL_EVENT": [],
+        "SECTION_DAV_XML_ID": null,
+        "DATE_FROM_FORMATTED": "Fri Oct 31 2025 18:00:00",
+        "DATE_TO_FORMATTED": "Fri Oct 31 2025 21:00:00",
+        "IS_DAYLIGHT_SAVING_TZ": "N",
+        "SECT_ID": "1949",
+        "OPTIONS": null,
+        "ATTENDEE_LIST": [
+            {
+                "id": 2857,
+                "entryId": "193694",
+                "status": "Y"
+            },
+            .....
+        ],
+        "COLLAB_ID": null,
+        "attendeesEntityList": [
+            {
+                "entityId": "user",
+                "id": 2857
+            },
+            {
+                "entityId": "meta-user",
+                "id": "all-users"
+            }
+        ],
+            "~USER_OFFSET_FROM": 0,
+            "~USER_OFFSET_TO": 0
+        }
+    ],
+    "time": {
+        "start": 1764830411,
+        "finish": 1764830411.63989,
+        "duration": 0.639888048171997,
+        "processing": 0,
+        "date_start": "2025-12-04T10:40:11+04:00",
+        "date_finish": "2025-12-04T10:40:11+04:00"
+    }
+    }
+
+    ```
+
+    #### Запрос:
+    ```
     GET /calendar/01.01.2024/01.01.2025
+    
+    ```
+
+    #### Вывод
+    ```
+    
     ```
 
     """

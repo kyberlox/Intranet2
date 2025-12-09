bba56110ad35a1ca7b08398d718d8ef4db05ffa7
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2025-12-09 11:25:31 +0400

    отладка MAXa

--- code/src/services/Auth.py
+++ code/src/services/Auth.py
@@ -435,7 +435,7 @@ def try_b24(login, password):
 @auth_router.get("/auth")
 async def authentication_b24(request: Request):
     print({"query_params": request.query_params, "req": request})
-    return None
+    return "a777aa777ru"
 
 @auth_router.post("/auth")
 async def authentication(response: Response, data=Body(), sess: AsyncSession = Depends(get_async_db)):

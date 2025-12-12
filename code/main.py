838ae27ec3d035c6e535381a1080e6032126cdad
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-12 13:29:03 +0300

    Документация по интеграции с Битрикс24

--- code/main.py
+++ code/main.py
@@ -2459,7 +2459,8 @@ async def custom_swagger_ui_html():
     # )
     
     # 5. Добавляем кастомные стили в head
-    modified_html = modified_html.replace('</head>', CUSTOM_CSS + '</head>')
+    #modified_html = modified_html.replace('</head>', CUSTOM_CSS + '</head>')
+    
     
     # 6. Возвращаем новый объект HTMLResponse с модифицированным содержимым
     return HTMLResponse(content=modified_html)

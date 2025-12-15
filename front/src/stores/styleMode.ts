e6cc9270aae1bb7e4fa453569e054bde856ce226
Fullofb3ans <igorgazinskii6@gmail.com>
2025-12-15 15:33:24 +0400

    iss/dark-style-default-light

--- front/src/stores/styleMode.ts
+++ front/src/stores/styleMode.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useStyleModeStore = defineStore('styleModeStore', {
     state: () => ({
-        darkMode: true,
+        darkMode: false,
     }),
 
     actions: {

9ad9a291280ee5bf5e3d5303df8279f034e29314
Fullofb3ans <igorgazinskii6@gmail.com>
2025-12-18 10:24:00 +0400

    bug/url-bug

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -581,7 +581,7 @@ const router = createRouter({
       path: '/oauthRedir',
       name: 'oauthPage',
       beforeEnter: (to, from, next) => {
-        window.location.href = `https://test-portal.emk.ru/oauth/authorize/?client_id=${oauthClient}&amp;redirect_uri=https%3A%2F%2F${oauthDomen}}%2Fintranet%2Frest%2Fauthuser.php&amp;response_type=code&amp;state=test_1765436150&amp;scope=user`;
+        window.location.href = `https://${oauthDomen}/oauth/authorize/?client_id=${oauthClient}&amp;redirect_uri=https%3A%2F%2F${oauthDomen}%2Fintranet%2Frest%2Fauthuser.php&amp;response_type=code&amp;state=test_1765436150&amp;scope=user`;
       },
       redirect: '',
     },

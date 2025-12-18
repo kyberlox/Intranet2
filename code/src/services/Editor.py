31f78068babc6ce00b602b760f524297ea88baf8
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-18 13:10:28 +0300

    репортажи

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -54,6 +54,14 @@ def make_date_valid(date):
                     # return datetime.datetime.strptime(date, '%d.%m.%Y %H:%M:%S')
                     return datetime.datetime.strptime(date, '%d.%m.%Y')
                     # return datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+            elif 'T' in date:
+                try:
+                    # return datetime.datetime.strptime(date, '%d.%m.%Y %H:%M:%S')
+                    return datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')
+                except:
+                    # return datetime.datetime.strptime(date, '%d.%m.%Y %H:%M:%S')
+                    # return datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+                    return datetime.datetime.strptime(date, '%Y-%m-%d')
 
 
 def get_type(value):

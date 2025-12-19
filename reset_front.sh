8f052e01fcc2843a132ee3b599d795dcafe034cc
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-19 11:23:10 +0300

    есть идея

--- reset_front.sh
+++ reset_front.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
-# docker-compose down
-# git pull origin main
-# docker images
-# docker rmi intranet_frontend
-# docker-compose up --build -d
-
-
-
 git pull origin main
-docker-compose stop vue_async
+docker-compose stop frontend
 docker rmi intranet_frontend
-docker-compose up -d --build vue_async
-
-
-
-# docker-compose rm fastapi
-# docker-compose up -d fastapi
-
-# docker-compose logs -f fastapi
+docker-compose up -d --build frontend

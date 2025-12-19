6193bc8e6720c406633f63ad653e7eda94dfcc9a
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-19 11:26:25 +0300

    есть идея

--- reset_front.sh
+++ reset_front.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+
 git pull origin main
+
+
 docker-compose stop frontend
-docker rmi intranet_frontend
-docker-compose up -d frontend
+docker-compose rm frontend
+docker-compose up -d --build frontend # если требуется установка библиотек
+
+
+# git pull origin main
+# docker-compose stop frontend
+# docker rmi intranet_frontend
+# docker-compose up -d frontend

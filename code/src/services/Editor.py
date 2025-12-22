b505b35409f8ddb179255d89c0a6095c975e62ae
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-22 17:10:28 +0400

    редакторка ссылок

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -805,6 +805,10 @@ class Editor:
             file_info["b24_url"] = file["b24_url"]
             url = file["file_url"]
 
+            print(file)
+
+            if file
+
             file_info["file_url"] = f"{DOMAIN}{url}"
 
             file_info["active"] = file["active"]

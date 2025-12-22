efcfa4d51b3006b230bc4f8d581ef7f5f17f7362
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-22 17:32:10 +0400

    редакторка ссылок

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -793,14 +793,17 @@ class Editor:
 
             # файлы делятся по категориям
             if "image" in file["content_type"]:
+                url = file_info["file_url"]
                 file_info["type"] = "image"
                 file_info["file_url"] = f"{DOMAIN}{url}"
             elif "video" in file["content_type"]:
+                url = file_info["file_url"]
                 file_info["file_url"] = f"{DOMAIN}{url}"
                 file_info["type"] = "video"
             elif "link" in file["content_type"]:
                 file_info["type"] = "video_embed"
             else:
+                
                 file_info["file_url"] = f"{DOMAIN}{url}"
                 file_info["type"] = "documentation"
 

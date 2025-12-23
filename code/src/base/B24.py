9ae1f67650b4795f751ff281f2926498e0e37d57
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-12-23 10:11:38 +0400

    идея

--- code/src/base/B24.py
+++ code/src/base/B24.py
@@ -61,6 +61,7 @@ class B24:
     async def get_file(self, id, inf_id):
         self.bx24 = Bitrix24("https://portal.emk.ru/rest/1/j6122m0ystded5ag/")
         result = await self.bx24.callMethod(f'disk.attachedObject.get?ENTITY_ID={inf_id}&id={id}')
+        print(f"https://portal.emk.ru/rest/1/j6122m0ystded5ag/disk.attachedObject.get?ENTITY_ID={inf_id}&id={id})
         return result
 
     async def get_all_files(self, id):

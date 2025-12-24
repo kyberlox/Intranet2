bc17f3011503104a92270f055923f17fea1159f0
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2025-12-24 10:13:48 +0400

    отладка MAXа

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -1967,8 +1967,9 @@ class Article:
             for res in result:
                 if res['active']:
                     if int(self.section_id) in [31, 16, 33]:
-                        if res["date_publiction"] is None or (
-                                "date_publiction" in res and res["date_publiction"] <= current_datetime):
+                        print(res["date_publiction"], current_datetime, 'ДАТЫ')
+                        if res["date_publiction"] is None or ("date_publiction" in res and res["date_publiction"] <= current_datetime):
+
                             self.id = res["id"]
 
                             # res["preview_file_url"] = await self.get_preview(session)

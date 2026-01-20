f46634e0303de719a1e23a73bcef480f4bab06fe
Fullofb3ans <igorgazinskii6@gmail.com>
2026-01-20 21:33:03 +0400

    bug/noEventOnMounted

--- front/src/views/innerCommunications/CorpEvents.vue
+++ front/src/views/innerCommunications/CorpEvents.vue
@@ -63,7 +63,7 @@ export default defineComponent({
         }, { immediate: true, deep: true })
 
         onMounted(() => {
-            if (!allEvents.value.length)
+            if (!allEvents.value.length && filterYears.value.length)
                 Api.get(`article/find_by/${sectionTips['КорпоративныеСобытия']}`)
                     .then(res => {
                         useViewsDataStore().setData(res, 'corpEventsData');

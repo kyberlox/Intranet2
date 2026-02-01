42da6a92791bf25a4f95c58e5eee6ba13e16cb0d
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-01 15:26:05 +0400

    Тестирую планировщик задач на дни рождения

--- code/src/services/scheduler.py
+++ code/src/services/scheduler.py
@@ -397,7 +397,7 @@ class AioSchedulerManager:
             # Добавляем задачи по умолчанию
             
             # 1. Ежедневная проверка каждые 5 минут (исправленный метод)
-            daily_job_id = self.schedule_periodic_task(daily_check, interval_seconds=300)
+            daily_job_id = self.schedule_periodic_task(daily_check, interval_seconds=120)
             
             # 2. Ежедневная проверка в 7 утра
             # daily_7am_job_id = self.schedule_daily_at_time(daily_check, hour=7, minute=0)

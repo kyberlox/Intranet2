ee63c58c70dcb863524468c3dc4b0e99b7b93a88
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-09 16:11:12 +0400

    отправка баллов пользователю за предложенную новость

--- code/src/base/pSQL/objects/PeerUserModel.py
+++ code/src/base/pSQL/objects/PeerUserModel.py
@@ -612,30 +612,30 @@ class PeerUserModel:
                 
                 value = 0
                 
-                if activities_id in roots.get("PeerCurator", []):
-                    session.add(new_action)
-                    # await session.commit()
-                    
-                    stmt_coast = select(self.Activities.coast).where(self.Activities.id == activities_id)
-                    result_coast = await session.execute(stmt_coast)
-                    value = result_coast.scalar()
-                    
-                    merch_model = MerchStoreModel(uuid_to)
-                    add_points = await merch_model.upload_user_sum(session, value)
-                    
-                    add_history = self.PeerHistory(
-                        user_uuid=roots['user_id'],
-                        user_to=uuid_to,
-                        active_info=description,
-                        active_coast=value,
-                        active_id=new_id,
-                        info_type='activity',
-                        date_time=datetime.now()
-                    )
-                    
-                    session.add(add_history)
-                    # await session.commit()
-                    return LogsMaker().info_message(f"Активность успешно отправлена пользователю с id = {uuid_to}")
+                # if activities_id in roots.get("PeerCurator", []):
+                session.add(new_action)
+                # await session.commit()
+                
+                stmt_coast = select(self.Activities.coast).where(self.Activities.id == activities_id)
+                result_coast = await session.execute(stmt_coast)
+                value = result_coast.scalar()
+                
+                merch_model = MerchStoreModel(uuid_to)
+                add_points = await merch_model.upload_user_sum(session, value)
+                
+                add_history = self.PeerHistory(
+                    user_uuid=roots['user_id'],
+                    user_to=uuid_to,
+                    active_info=description,
+                    active_coast=value,
+                    active_id=new_id,
+                    info_type='activity',
+                    date_time=datetime.now()
+                )
+                
+                session.add(add_history)
+                # await session.commit()
+                return LogsMaker().info_message(f"Активность успешно отправлена пользователю с id = {uuid_to}")
                 
                 # else:
                 #     return LogsMaker().warning_message(f"Недостаточно прав для отправки активности")

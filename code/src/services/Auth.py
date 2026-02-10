11f1e21bca6d9431e7380bd664a0a59b3c398675
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-02-10 16:04:22 +0400

    авторизация контактов с выставок

--- code/src/services/Auth.py
+++ code/src/services/Auth.py
@@ -899,25 +899,20 @@ async def tepconf(request: Request, session_data: Dict[str, Any] = Depends(get_c
         'position' : session_data['user_info']['WORK_POSITION']
     }
     
-    async with httpx.AsyncClient(timeout=30.0) as client:
-        res = await client.post(
-            url='http://exhibitions.kuberlox.ru/login',
-            json=user_info
+    res = requests.post(url='http://exhibitions.kuberlox.ru/login', json=user_info)
+
+    if res.status_code != 200:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST ,
+            detail=f"Ошибка перехода на контакты с выставок: {res.text}"
         )
-        
-        # Проверьте ответ
-        if res.status_code != 200:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST ,
-                detail=f"Ошибка перехода на контакты с выставок: {res.text}"
-            )
-    
 
-    redirect_url = f"http://exhibitions.kuberlox.ru/login"
-     # Создаем RedirectResponse
-    response = RedirectResponse(url=redirect_url) #, status_code=302
 
-    return response
+    # redirect_url = f"http://exhibitions.kuberlox.ru/login"
+    #  # Создаем RedirectResponse
+    # response = RedirectResponse(url=redirect_url) #, status_code=302
+
+    return res.json
 
 
 

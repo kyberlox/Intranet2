1f7e6b86d8c333d8a5cf43fbe62a7b46c8b11096
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-11 11:26:14 +0400

    отладка автора в редакторке

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -1041,7 +1041,7 @@ class Editor:
             art_fields = fields_to_return[str(art['section_id'])]
             print(art['indirect_data'], 'до')
             if art['section_id'] == 31:
-                await Peer(user_uuid=art['indirect_data']['author']['id']).remove_author_points(session=session, article_id=int(self.art_id))
+                await Peer(user_uuid=art['indirect_data']['author']['id']).remove_author_points(session=self.session, article_id=int(self.art_id))
                 print(art['indirect_data'], 'до')
                 art['indirect_data'].pop('author')
                 print(art['indirect_data'], 'после')

760eeda619dc1a6378d287141759452c408b6fd6
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-11 10:13:58 +0400

    отладка автора в редакторке

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -875,7 +875,7 @@ class Editor:
 
     async def get_users_info(self, user_id_list):
         await self.validate()
-        art = await Article(id=self.art_id).find_by_id(self.session)
+        art = await Article(id=int(self.art_id)).find_by_id(self.session)
 
         if user_id_list == []:
             art['indirect_data']['users'] = []
@@ -1036,7 +1036,7 @@ class Editor:
             ]
         }
         if not user_id:
-            art = await Article(id=self.art_id).find_by_id(self.session)
+            art = await Article(id=int(self.art_id)).find_by_id(self.session)
             art_fields = fields_to_return[str(art['section_id'])]
             if art['section_id'] == 31:
                 art['indirect_data'].pop('author')
@@ -1174,7 +1174,7 @@ async def get_editor_roots(user_uuid, session):
 
 
 @editor_router.get("/get_user_info/{section_id}/{art_id}/{user_id}")
-async def set_user_info(section_id: int, art_id: int, user_id: int, session: AsyncSession = Depends(get_async_db)):
+async def set_user_info(section_id: int, art_id: int, user_id: Optional[int] = None, session: AsyncSession = Depends(get_async_db)):
     return await Editor(art_id=art_id, section_id=section_id, session=session).get_user_info(user_id)
 
 

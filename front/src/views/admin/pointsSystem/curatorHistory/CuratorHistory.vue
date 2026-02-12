afd945fee41b33a80562f497c6b321cca32ddbfd
Fullofb3ans <igorgazinskii6@gmail.com>
2026-02-12 16:30:18 +0400

    bug/year-results-bug

--- front/src/views/admin/pointsSystem/curatorHistory/CuratorHistory.vue
+++ front/src/views/admin/pointsSystem/curatorHistory/CuratorHistory.vue
@@ -1,4 +1,13 @@
-<template>
+<template><!-- <div v-if="needFilter">
+    <button @click="showFilter = !showFilter"
+            class="btn dropdown-toggle tagDateNavBar__dropdown-toggle">
+        Активность
+    </button>
+    <CustomFilter v-if="showFilter"
+                  :params="filterYears"
+                  :buttonText="currentYear ?? 'Год'"
+                  @pickFilter="(year: string) => currentYear = year" />
+</div> -->
 <PointsHistoryActionTable :needCheckButton="false"
                           :onlyHistory="true"
                           :activitiesInTable="activitiesInTable"
@@ -10,12 +19,22 @@ import { defineComponent, onMounted, ref } from 'vue';
 import PointsHistoryActionTable from '../PointsHistoryActionTable.vue';
 import type { ICuratorActivityHistory } from '@/interfaces/IEntities';
 import Api from '@/utils/Api';
+import CustomFilter from '@/components/tools/common/CustomFilter.vue';
+
 export default defineComponent({
     components: {
-        PointsHistoryActionTable
+        PointsHistoryActionTable,
+        CustomFilter
+    },
+    props: {
+        needFilter: {
+            type: Boolean,
+            default: () => false
+        }
     },
     setup() {
         const activitiesInTable = ref<ICuratorActivityHistory[]>([]);
+        const showFilter = ref(false);
 
         onMounted(() => {
             tableInit();
@@ -29,10 +48,29 @@ export default defineComponent({
         const tableInit = () => {
             Api.get('peer/get_curators_history')
                 .then((data: ICuratorActivityHistory[]) => activitiesInTable.value = data)
+            // .finally(() => getFilterTypes(activitiesInTable.value))
         }
+
+        // const getFilterTypes = (data: ICuratorActivityHistory[]) => {
+        //     const filterKeys = ["activity_name", "uuid_to_fio", "description"];
+        //     const filterObject: { activity_name?: string[], uuid_to_fio?: string[], description?: string[] } = {};
+        //     filterKeys.forEach((e) => {
+        //         data.forEach((i) => {
+        //             if ((e as 'activity_name' | 'uuid_to_fio' | 'description') in filterObject && !(filterObject[e].includes(i[e as ('activity_name' | 'uuid_to_fio' | 'description') as keyof ICuratorActivityHistory]))) {
+        //                 filterObject[(e as 'activity_name' | 'uuid_to_fio' | 'description')].push(i[e as keyof ICuratorActivityHistory])
+        //             }
+        //             else if (!(e in filterObject)) {
+        //                 console.log(2);
+        //                 filterObject[e as 'activity_name' | 'uuid_to_fio' | 'description'].push(i[e as keyof ICuratorActivityHistory])
+        //             }
+        //         })
+        //     })
+        //     console.log(filterObject);
+        // }
 
         return {
             activitiesInTable,
+            showFilter,
             moderate
         }
     }

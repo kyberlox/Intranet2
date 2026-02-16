4ac19beb370f01f61e66f611265e972d055b4e8f
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-16 10:42:38 +0400

    тестируем бажанову

--- code/src/model/User.py
+++ code/src/model/User.py
@@ -244,8 +244,8 @@ class User:
                 "description": ""
             }
             print(123)
-            self.UserModel.Id = 2112
-            user = await self.UserModel.find_by_id(session)
+            # self.UserModel.Id = 2112
+            user = await self.UserModel(Id = 2112).find_by_id(session)
             print(user)
             # for user in all_users:
             #     if user.active is True:

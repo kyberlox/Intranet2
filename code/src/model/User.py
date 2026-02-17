4872e7dbedaa9aa36807dccce959d6543e85f016
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-17 10:20:04 +0400

    отладка авторизации

--- code/src/model/User.py
+++ code/src/model/User.py
@@ -650,9 +650,10 @@ class User:
                     # if "name" in user_inf and "last_name" in user_inf: 
                     ws[f'B{row_number}'] = f'{user_inf.last_name} {user_inf.name} {user_inf.second_name}'
 
-                    if 'uf_department' in indirect_data:
-                        ped_info = await Department(id=indirect_data['uf_department'][0]).search_dep_by_id(session)
-                        ws[f'C{row_number}'] = f'{ped_info[0].name}'
+                    if 'uf_department' in indirect_data and isinstance(indirect_data['uf_department'], list):
+                        if len(indirect_data['uf_department']) >= 1:
+                            ped_info = await Department(id=indirect_data['uf_department'][0]).search_dep_by_id(session)
+                            ws[f'C{row_number}'] = f'{ped_info[0].name}'
                     
                     # if 'personal_city' in user_inf:
                     ws[f'D{row_number}'] = f'{user_inf.personal_city}'

d691dd11feb8394eb56a26cadd259d084903c0de
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-17 12:05:14 +0400

    отладка сессий

--- code/src/services/Auth.py
+++ code/src/services/Auth.py
@@ -304,9 +304,9 @@ class AuthService:
     def delete_session(self, session_id: str, key: Optional[str] = None) -> None:
         """Удаление сессии"""
         session_data = self.redis.get_session(session_id)
-        print('пытаемся удалить?')
+        
         # if session_data and "user_id" in session_data:
-        print('удаляем?')
+        
         # Удаляем session_id из списка сессий пользователя
         # user_sessions_key = f"user_sessions:{session_data['user_id']}"
         self.redis.remove_from_set(key, session_id)

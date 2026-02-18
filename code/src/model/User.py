3f192dc261a7891917ed56b850ee13001a7c2fbb
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-18 10:00:14 +0400

    отладка файла со статистикой

--- code/src/model/User.py
+++ code/src/model/User.py
@@ -683,10 +683,10 @@ class User:
                                 visits = json.loads(res)
                                 data_stat = visits['totals']
 
-                                ws[f'F{row_number}'] = f'{data_stat['totals'][0]}'
-                                ws[f'G{row_number}'] = f'{data_stat['totals'][1]}'
+                                ws[f'F{row_number}'] = f'{data_stat[0]}'
+                                ws[f'G{row_number}'] = f'{data_stat[1]}'
 
-                                avg_time_min = data_stat['totals'][2] / 60
+                                avg_time_min = data_stat[2] / 60
                                 ws[f'H{row_number}'] = f'{avg_time_min}'
                         #ставим таймаут
                         # await asyncio.sleep(2)

390e68498c0d55b2313c743cd8cba54fc6b02a90
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-02-18 12:26:35 +0400

    авторизация для APK

--- code/src/services/Auth.py
+++ code/src/services/Auth.py
@@ -986,7 +986,7 @@ async def tepconf(request: Request, session_data: Dict[str, Any] = Depends(get_c
     ID = session_data['user_id']
     session_id = session_data["session_id"]
     position = session_data['user_info']['WORK_POSITION']
-    response = RedirectResponse(url=f"test_app://auth?id={ID}&session_id={session_id}&fio={fio}&department={department}&position={position}", status_code=302)
+    response = RedirectResponse(url=f"contacts_app_emk://auth?id={ID}&session_id={session_id}&fio={fio}&department={department}&position={position}", status_code=302)
 
     
     

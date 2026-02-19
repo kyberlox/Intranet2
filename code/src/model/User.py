d05859bfe85a559e600f4c26049ad2d0e74cd184
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-19 09:50:51 +0400

    отладка файла со статистикой

--- code/src/model/User.py
+++ code/src/model/User.py
@@ -669,9 +669,10 @@ class User:
                                     ws[f'C{row_number}'] = f'{ped_info[0].name}'
                         
                         # if 'personal_city' in user_inf:
+                        if user_inf.personal_city:
                         ws[f'D{row_number}'] = f'{user_inf.personal_city}'
                         
-                        if 'work_position' in indirect_data:
+                        if 'work_position' in indirect_data and indirect_data['work_position']:
                             ws[f'E{row_number}'] = f'{indirect_data['work_position']}'
 
                         data_stat = []

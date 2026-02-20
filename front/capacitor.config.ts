7e82b7e6d9959ec3fcdadb2b1e3fa9c5f920e099
Fullofb3ans <igorgazinskii6@gmail.com>
2026-02-20 16:30:37 +0400

    iss/change homeview blank route

---
+++ front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ru.emk.intranet',
+  appName: 'intranetEMK',
+  webDir: 'dist'
+};
+
+export default config;

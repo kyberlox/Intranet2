d9573d87fa5ddacc08f742fe9552a2ba61546308
Fullofb3ans <igorgazinskii6@gmail.com>
2026-02-20 17:53:23 +0400

    iss/cap-apk

--- front/capacitor.config.ts
+++ front/capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ru.emk.intranet',
   appName: 'intranetEMK',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    allowNavigation: [
+      'intranet.emk.ru',
+      'intranet.emk.org.ru',
+      'portal.emk.ru',
+      'test-portal.emk.ru'
+    ]
+  }
 };
 
 export default config;

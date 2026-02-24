aba492a24034caf03b99206f1372f21256c0cf60
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-24 16:38:02 +0400

    Тестирую несколько авторов на выдачу баллов

--- code/src/model/User.py
+++ code/src/model/User.py
@@ -726,6 +726,9 @@ class User:
                     if 'date_of_employment' not in user['indirect_data']:
                         if user['id'] in [2, 508]:
                             continue
+                        convert_date = make_date_valid(user['indirect_data']['date_register'])
+                        date_of_employment = datetime.strftime(convert_date, '%d.%m.%Y')
+                        user['indirect_data']['date_of_employment'] = date_of_employment
                         is_employment_exist_count.append(user)
                         continue
                     if 'date_of_employment' in user['indirect_data'] and user['indirect_data']['date_of_employment'] == '':

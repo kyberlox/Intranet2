bc1a8d235566bba59d57137ac522f6ef91de37d2
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-25 14:52:12 +0400

    отладка авторизации в exhibition

--- code/src/services/Auth.py
+++ code/src/services/Auth.py
@@ -913,7 +913,7 @@ async def tepconf(request: Request, session_data: Dict[str, Any] = Depends(get_c
     }
 
     print(user_info)
-    async with httpx.AsyncClient as client:
+    async with httpx.AsyncClient(timeout=30.0) as client:
         res = await client.post(url='http://exhibitions.kyberlox.ru/login', json=user_info)
     
 

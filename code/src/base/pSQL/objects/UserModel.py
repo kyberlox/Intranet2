4bde817a868ab9812d1f2fe000b9d20e4ddb4830
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-03-12 14:16:23 +0400

    Добавление названий предприятий под фотками сотрудников предприятий

--- code/src/base/pSQL/objects/UserModel.py
+++ code/src/base/pSQL/objects/UserModel.py
@@ -600,7 +600,7 @@ class UserModel:
 
                             dep_str = await DepartmentModel(dep).find_dep_by_id(session)  # как обложим асинхронностью добавить эвэйт!!!!!!!!!!!!!!!!!!!
                             for de in dep_str:
-                                if de.id in manufactures or :
+                                if de.id in manufactures:
                                     # user_info['location'] = manufactures[de.id]
                                     if manufactures[de.id] not in list_departs:
                                         list_departs.append(manufactures[de.id])

c193124a0241ab30fbec5739177bdf444d8aaabd
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-03-12 20:10:26 +0400

    вернул старую сортировку памятки

--- code/src/model/Article.py
+++ code/src/model/Article.py
@@ -2025,9 +2025,9 @@ class Article:
                 sorted_active_articles = sorted(active_articles, key=lambda x: x['name'], reverse=False)
             # отдельная сортировка Памятки новому сторуднику
             elif self.section_id == "18":
-                sorted_active_articles = sorted(active_articles, key=lambda x: x['date_publiction'], reverse=False)
+                # sorted_active_articles = sorted(active_articles, key=lambda x: x['date_publiction'], reverse=False)
             # elif self.section_id == "18":
-            #     sorted_active_articles = sorted(active_articles, key=lambda x: int(x['indirect_data']["sort"]), reverse=False)
+                sorted_active_articles = sorted(active_articles, key=lambda x: int(x['indirect_data']["sort"]), reverse=False)
             elif int(self.section_id) in SECTIONS_WITH_DATE_PUBLICTION:
                 sorted_active_articles = sorted(active_articles, key=lambda x: x['date_publiction'], reverse=True)
             else:

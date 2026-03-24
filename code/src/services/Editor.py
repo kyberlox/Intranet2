4e01d7bb162d24c9ea2f0a0a1186043fe3960b3e
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-03-24 14:23:32 +0400

    обновляем области видимости

--- code/src/services/Editor.py
+++ code/src/services/Editor.py
@@ -543,7 +543,7 @@ class Editor:
                 field.append(fl)
 
         got_fields = field
-
+        print(got_fields)
         # photo_file_url нужен только там, где он есть
         # for f, i in enumerate(field):
         #     if field["field"] == "photo_file_url" and field["value"] is None:
@@ -555,7 +555,7 @@ class Editor:
         for need_field in curr_patterns["fields"]:
             has_added = False
             for got_field in got_fields:
-                print(got_field["field"], 123)
+                # print(got_field["field"], 123)
                 # если такое поле есть среди заполненных
                 if need_field["field"] == got_field["field"]:
 
@@ -590,11 +590,11 @@ class Editor:
                     # sorted_tags = sorted(tags_list, key=lambda x: x.tag_name, reverse=False)
                     need_field["values"] = fields_list
                 
-                # if need_field["field"] == "vision":
-                #     fields_list = await Visions(art_id=self.art_id).get_all_vis_in_art(self.session)
+                # if need_field["field"] == "vision_select":
+                #     # fields_list = await Visions(art_id=self.art_id).get_all_vis_in_art(self.session)
                 #     # sorted_tags = sorted(tags_list, key=lambda x: x.tag_name, reverse=False)
                 #     need_field["values"] = fields_list
-                print(need_field["field"], 234)
+                # print(need_field["field"], 234)
                 result_fields.append(need_field)
 
         # вытащить файлы

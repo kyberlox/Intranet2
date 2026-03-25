942fc09620a58242fb00ba883b9a70e4c29e7bae
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-03-25 16:15:14 +0400

    пробуем

--- code/src/base/pSQL/objects/UservisionsRootModel.py
+++ code/src/base/pSQL/objects/UservisionsRootModel.py
@@ -153,14 +153,73 @@ class UservisionsRootModel:
             existing_vision = res.scalar_one_or_none()
             if existing_vision:
                 # users_in_vis = database.query(UservisionsRoot).filter(UservisionsRoot.vision_id == self.vision_id).all()
-                query = select(self.Roots.user_uuid).where(
-                        self.Roots.root_token['VisionRoots'].astext.cast(JSONB).contains([self.vision_id])
+                query = select(
+                    self.Roots.user_uuid
+                ).where(
+                    self.Roots.root_token['VisionRoots'].astext.cast(JSONB).contains([self.vision_id])
                 )
 
 
                 res = await session.execute(query)
                 users_in_vis = res.scalars().all()
                 for user in users_in_vis:
+                    general_info = {}
+                    user_info = await UserModel(Id=user).find_by_id(session=session)
+                    if user_info['active']:
+                        general_info['id'] = user_info['id']
+                        name = user_info['name'] if user_info['name'] else ''
+                        last_name = user_info['last_name'] if user_info['last_name'] else ''
+                        second_name = user_info['second_name'] if user_info['second_name'] else ''
+                        general_info['name'] = last_name + ' ' + name + ' ' + second_name
+                        general_info['depart'] = user_info['indirect_data']['uf_department'][0] if 'uf_department' in user_info['indirect_data'].keys() else None
+                        general_info['depart_id'] = user_info['indirect_data']['uf_department_id'][0] if 'uf_department_id' in user_info['indirect_data'].keys() else None
+                        if general_info['depart_id']:
+                            res_manufacture = await self.get_user_manufacture(dep_id=general_info['depart_id'], manufactures=manufactures, session=session)
+                            if res_manufacture:
+                                general_info['depart'] = f"{general_info['depart']} | {manufactures[res_manufacture]}"
+                                # general_info['father_depart_name'] = manufactures[int(res_manufacture)]
+                        if 'work_position' in user_info['indirect_data'].keys():
+                            general_info['post'] = user_info['indirect_data']['work_position']
+                        general_info['image'] = user_info['photo_file_url'] if 'photo_file_url' in user_info.keys() else None
+                        result.append(general_info)
+                return result
+            return LogsMaker().warning_message(f"ОВ с id = {self.vision_id} не существует")
+        except Exception as e:
+
+            return LogsMaker().error_message(f"ошибка при выводе пользователей из ОВ {self.vision_id}: {e}")
+    
+    async def find_users_in_vision(self, session):
+        manufactures = await self.get_manufactures_id(session)
+        from ..models.Department import Department
+        from .UserModel import UserModel
+        try:
+            result = []
+            stmt = select(self.Fieldvision).where(self.Fieldvision.id == self.vision_id)
+            res = await session.execute(stmt)
+            existing_vision = res.scalar_one_or_none()
+            if existing_vision:
+                # users_in_vis = database.query(UservisionsRoot).filter(UservisionsRoot.vision_id == self.vision_id).all()
+                query = select(
+                    self.User.id,
+                    self.User.name,
+                    self.User.last_name,
+                    self.User.second_name,
+                    self.User.indirect_data['uf_department'][0],
+                    self.User.indirect_data['work_position'],
+                    Department.name                    
+                ).join(
+                    self.User, self.User.id == self.Roots.user_uuid
+                ).join(
+                    Department, Department.id == self.User.indirect_data['uf_department'][0]
+                ).where(
+                    self.Roots.root_token['VisionRoots'].astext.cast(JSONB).contains([self.vision_id])
+                )
+
+
+                res = await session.execute(query)
+                users_in_vis = res.scalars().all()
+                for user in users_in_vis:
+                    print(user, 123)
                     general_info = {}
                     user_info = await UserModel(Id=user).find_by_id(session=session)
                     if user_info['active']:

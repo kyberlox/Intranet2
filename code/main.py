edb9ff7d033a18d70dfb3dbe909189944160c84d
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-04-07 13:04:44 +0400

    снова пробуем графану

--- code/main.py
+++ code/main.py
@@ -148,7 +148,7 @@ app.mount("/api/vcard_files", StaticFiles(directory='./vcard_db'), name="vcard_f
 #Пробуем метрики
 instrumentator = Instrumentator()
 instrumentator.add(lambda info: {"app_name": "my_app"})  # метка app_name
-instrumentator.instrument(app).expose(app))
+instrumentator.instrument(app).expose(app)
 
 # Исключаем эндпоинты, которые не требуют авторизации (например, сам эндпоинт авторизации)
 open_links = [

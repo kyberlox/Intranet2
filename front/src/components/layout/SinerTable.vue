fadf29676579f99e3a762954bcf525180b42b26b
= <=>
2026-04-27 12:09:01 +0400

    FEAT: заменил Loader синертим

--- front/src/components/layout/SinerTable.vue
+++ front/src/components/layout/SinerTable.vue
@@ -28,6 +28,7 @@
             </tr>
         </thead>
 
+<<<<<<< HEAD
         <tbody v-if="!loading">
             <tr v-for="(row, rowIndex) in sortedRows"
                 :key="rowIndex"
@@ -65,6 +66,52 @@
 <div class="siner-table__loading">
     <Loader />
 </div>
+=======
+            <tbody v-if="!loading">
+                <tr
+                    v-for="(row, rowIndex) in sortedRows"
+                    :key="rowIndex"
+                    @click="handleRowClick(row, rowIndex)"
+                >
+                    <td v-for="column in columns" :key="column.key">
+                        <slot
+                            :name="`column-${column.key}`"
+                            :value="getValue(row, column.key)"
+                            :row="row"
+                            :column="column"
+                            :rowIndex="rowIndex"
+                        >
+                            <template v-if="column.formatter">
+                                {{ column.formatter(getValue(row, column.key)) }}
+                            </template>
+                            <span v-else-if="column.component">
+                                <component
+                                    :is="column.component"
+                                    v-bind="
+                                        getComponentProps(column, getValue(row, column.key), row)
+                                    "
+                                />
+                            </span>
+                            <template v-else>
+                                {{ formatValue(getValue(row, column.key)) }}
+                            </template>
+                        </slot>
+                    </td>
+                </tr>
+                <tr v-if="sortedRows.length === 0">
+                    <td :colspan="columns.length" class="siner-table__empty-cell">
+                        {{ emptyMessage }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="siner-table__loading-wrapper">
+        <div v-if="loading" class="siner-table__loading">
+            <Loader/>
+        </div>
+    </div>
+>>>>>>> d01ca02b (FEAT: Заменил Loader в синертим)
 </template>
 
 <script lang="ts">
@@ -119,6 +166,9 @@ export default defineComponent({
     },
 
     emits: ['row-click', 'cell-click', 'sort'],
+    components:{
+        Loader
+    },
 
     setup(props, { emit }) {
         const sortKey = ref<string | null>(null)

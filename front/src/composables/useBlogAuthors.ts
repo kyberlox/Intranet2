97b7cd59fcd01bad1943f5940aebe80c59cafae9
Fullofb3ans <igorgazinskii6@gmail.com>
2026-05-29 14:53:16 +0400

    bug/blog-sort

--- front/src/composables/useBlogAuthors.ts
+++ front/src/composables/useBlogAuthors.ts
@@ -6,28 +6,29 @@ import { useblogDataStore } from "@/stores/blogData"
 export const getBlogAuthorsToStore = async () => {
     const blogsData = useblogDataStore();
     const uniqAuthors: IBlogAuthors[] = [];
-    try {
-        const res = await Api.get(`article/find_by/${sectionTips['Блоги']}`)
-        res.map((e: IBlog) => {
-            if (e.indirect_data?.TITLE) {
-                const newAuthor: IBlogAuthors = {
-                    title: e.indirect_data.TITLE,
-                    authorId: e.indirect_data.author_uuid ?? e.indirect_data.company,
-                    authorAvatar: e.indirect_data.photo_file_url ?? e.preview_file_url,
-                    authorTitle: e.indirect_data.author,
-                    link: e.indirect_data.link ?? null,
-                    // !!! Сечас в preview_file_url приходят заводы, а в photo_file_url - фото людей, у земской приходит и preview, в нем QR !!!
-                    telegramQr: e.preview_file_url && e.indirect_data.photo_file_url ? e.preview_file_url : ''
+    if (!blogsData.getAllAuthors.length)
+        try {
+            const res = await Api.get(`article/find_by/${sectionTips['Блоги']}`)
+            res.map((e: IBlog) => {
+                if (e.indirect_data?.TITLE) {
+                    const newAuthor: IBlogAuthors = {
+                        title: e.indirect_data.TITLE,
+                        authorId: e.indirect_data.author_uuid ?? e.indirect_data.company,
+                        authorAvatar: e.indirect_data.photo_file_url ?? e.preview_file_url,
+                        authorTitle: e.indirect_data.author,
+                        link: e.indirect_data.link ?? null,
+                        // !!! Сечас в preview_file_url приходят заводы, а в photo_file_url - фото людей, у земской приходит и preview, в нем QR !!!
+                        telegramQr: e.preview_file_url && e.indirect_data.photo_file_url ? e.preview_file_url : ''
+                    }
+                    if (!uniqAuthors.length || !uniqAuthors.find((e) => e.title == newAuthor.title)) {
+                        uniqAuthors.push(newAuthor)
+                    }
                 }
-                if (!uniqAuthors.length || !uniqAuthors.find((e) => e.title == newAuthor.title)) {
-                    uniqAuthors.push(newAuthor)
-                }
-            }
-        })
-        blogsData.setAllBlogs(res);
-        blogsData.setAuthors(uniqAuthors);
-    }
-    catch (error) {
-        console.error(error)
-    }
+            })
+            blogsData.setAllBlogs(res);
+            blogsData.setAuthors(uniqAuthors);
+        }
+        catch (error) {
+            console.error(error)
+        }
 }

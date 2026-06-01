16947e1473beb4fc49bd8db3215609e408cb5071
Fullofb3ans <igorgazinskii6@gmail.com>
2026-06-01 09:52:36 +0400

    iss/newIdHandleMetrika

--- front/src/components/tools/common/YandexMetrika.vue
+++ front/src/components/tools/common/YandexMetrika.vue
@@ -14,13 +14,14 @@ export default defineComponent({
         }
     },
     setup(props) {
-        const ymKey = Number(import.meta.env.YMETRIKA_KEY);
-        (function (m, e, t, r, i, k, a) {
-            m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-            m[i].l = 1 * new Date();
-            for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-        })
+        const ymKey = (import.meta.env.VITE_YMETRIKA_KEY);
+        console.log(ymKey)
+            (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+                m[i].l = 1 * new Date();
+                for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
         ym(ymKey, "init", {

06856325970b2f3486d3fe6e4bf9801c8a1667bd
Fullofb3ans <igorgazinskii6@gmail.com>
2026-06-05 14:44:55 +0400

    bug/usersVal-blogs-only

--- front/src/utils/objectUtil.ts
+++ front/src/utils/objectUtil.ts
@@ -1,5 +1,7 @@
 import { type IUserList } from "@/components/tools/common/SearchList.vue";
 export const findValInObject = (searhIn: { fields: { name: string, field: string, value: string | string[] | IUserList[] }[] }, dataToSearch: string) => {
       const res = searhIn.fields.find((e: { field: string }) => e.field == dataToSearch)?.value;
-      return dataToSearch == 'users' && !Array.isArray(res) ? [res] : res
+      if (!res) return
+
+      return (dataToSearch == 'users' && !Array.isArray(res)) ? [res] : res
 }

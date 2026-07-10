b60d890648f34833522943b174b18f24009599e3
Fullofb3ans <igorgazinskii6@gmail.com>
2026-07-10 09:58:38 +0400

    bug/поправил дубли в орг развитии

--- front/src/components/layout/LayoutPostsPreview.vue
+++ front/src/components/layout/LayoutPostsPreview.vue
@@ -135,12 +135,17 @@ export default defineComponent({
             const paginationQuery = paginationEnabled.value ? `?offset=${offset.value}&limit=15${currentYear.value ? `&year=${currentYear.value}` : ''}${currentTag.value ? `&tag=${currentTag.value}` : ''}` : '';
             try {
                 const res = await Api.get(`article/find_by/${props.sectionId}${paginationQuery}`, null, abortController.signal)
-                if (offset.value == 0) { viewsData.setData(res, props.storeItemsName) }
+                if (offset.value == 0) {
+                    viewsData.setData(res, props.storeItemsName)
+                }
+                else {
+                    viewsData.setData(allNews.value.concat(res), props.storeItemsName)
+                }
                 if (res.length == 0 || res.length < 15) {
                     isLimit.value = true;
                 }
                 offset.value += 15;
-                visibleNews.value = visibleNews.value.concat(res);
+                visibleNews.value = allNews.value
             } catch (error) {
                 console.error(error)
             }

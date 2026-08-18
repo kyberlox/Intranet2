dc73d0724c81b82af6264f2c36cee0a14244b6c3
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-08-18 12:31:58 +0400

    СОЗДАЮ ТРАЗАНКЦИИ БАЛЛОВ МЕЖДУ ПОЛЬЗОВАТЕЛЯМИ

--- code/src/base/pSQL/objects/ActiveUsersModel.py
+++ code/src/base/pSQL/objects/ActiveUsersModel.py
@@ -436,9 +436,9 @@ class ActiveUsersModel:
             result_transaction = await session.execute(stmt_transaction)
             transaction_history = result_transaction.scalars().all()
 
-            # if not transaction_history:
-            #     sorted_result = sorted(activities, key=lambda x: x['date_time'], reverse=True)
-            #     return sorted_result
+            if not transaction_history:
+                sorted_result = sorted(activities, key=lambda x: x['date_time'], reverse=True)
+                return sorted_result
 
             # for transaction in transaction_history:
             #     your_id = self.uuid_to

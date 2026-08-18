1d213787156c88f89a9d5502878443f52e41f6c7
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-08-18 12:14:33 +0400

    СОЗДАЮ ТРАЗАНКЦИИ БАЛЛОВ МЕЖДУ ПОЛЬЗОВАТЕЛЯМИ

--- code/src/base/pSQL/objects/ActiveUsersModel.py
+++ code/src/base/pSQL/objects/ActiveUsersModel.py
@@ -441,28 +441,31 @@ class ActiveUsersModel:
             if not transaction_history:
                 sorted_result = sorted(activities, key=lambda x: x['date_time'], reverse=True)
                 return sorted_result
+
             for transaction in transaction_history:
-                your_id = self.uuid_to
-                if your_id == transaction.user_uuid:
-                    your_coast = -transaction.merch_coast
-                    another_user_id = transaction.user_to
-                    user_fio = await self.get_user_fio_by_id(another_user_id)
-                    another_user_fio = f"Вы"
-                    message  = f"Перевод баллов на сумму {transaction.merch_coast} \n Получатель  - {user_fio}"
-                else: #ты - получатель
-                    your_coast = transaction.active_coast
-                    another_user_id = transaction.user_uuid
-                    another_user_fio = await self.get_user_fio_by_id(another_user_id)
-                    message = f"Перевод бвллов на сумму {transaction.merch_coast} \n Отправитель  - {user_fio}" + transaction.description
-                activities.append({
-                    "id": transaction.id,
-                    "user_uuid": another_user_id,
-                    "fio_from": another_user_fio,
-                    "description": message,
-                    "date_time": merch.date_time,
-                    "activity_name": "Перевод",
-                    "cost": your_coast
-                })
+                print(transaction.user_uuid)
+                # your_id = self.uuid_to
+                # if your_id == transaction.user_uuid:
+                #     your_coast = -transaction.merch_coast
+                #     another_user_id = transaction.user_to
+                #     user_fio = await self.get_user_fio_by_id(another_user_id)
+                #     another_user_fio = f"Вы"
+                #     message  = f"Перевод баллов на сумму {transaction.merch_coast} \n Получатель  - {user_fio}"
+                # else: #ты - получатель
+                #     your_coast = transaction.active_coast
+                #     another_user_id = transaction.user_uuid
+                #     another_user_fio = await self.get_user_fio_by_id(another_user_id)
+                #     message = f"Перевод бвллов на сумму {transaction.merch_coast} \n Отправитель  - {user_fio}" + transaction.description
+
+                # activities.append({
+                #     "id": transaction.id,
+                #     "user_uuid": another_user_id,
+                #     "fio_from": another_user_fio,
+                #     "description": message,
+                #     "date_time": merch.date_time,
+                #     "activity_name": "Перевод",
+                #     "cost": your_coast
+                # })
 
             sorted_result = sorted(activities, key=lambda x: x['date_time'], reverse=True)
             return sorted_result
